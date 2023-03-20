6b18504975471e2b301517df0f6d01db8d6a2dec
KalpeshLambade <kalpeshkishorlambade@gmail.com>
2023-03-20 18:16:33 +0530

    updated

---
+++ netflix/src/Component/Homepage.jsx
@@ -0,0 +1,27 @@
+import "./Component.css";
+function Homepage() {
+  return (
+    <div>
+      <div className="home-top">
+      </div>
+      <div className="home-top-on">
+          <div className="home-top-nav">
+            <div>
+              <img
+                src="https://www.freepnglogos.com/uploads/netflix-logo-0.png"
+                alt="netflix logo"
+              />
+            </div>
+            <div>
+                <div>
+                    
+                </div>
+                <button>Sign In</button>
+            </div>
+          </div>
+        </div>
+    </div>
+  );
+}
+
+export default Homepage;
