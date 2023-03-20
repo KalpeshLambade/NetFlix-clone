59d0bdce58822d293dca057fa2d6b1c3c9cf7885
KalpeshLambade <kalpeshkishorlambade@gmail.com>
2023-03-20 19:03:49 +0530

    updated

--- netflix/src/Component/Homepage.jsx
+++ netflix/src/Component/Homepage.jsx
@@ -2,24 +2,41 @@ import "./Component.css";
 function Homepage() {
   return (
     <div>
-      <div className="home-top">
-      </div>
+      <div className="home-top"></div>
       <div className="home-top-on">
-          <div className="home-top-nav">
+        <div className="home-top-nav">
+          <div>
+            <img
+              src="https://www.freepnglogos.com/uploads/netflix-logo-0.png"
+              alt="netflix logo"
+            />
+          </div>
+          <div>
             <div>
-              <img
-                src="https://www.freepnglogos.com/uploads/netflix-logo-0.png"
-                alt="netflix logo"
-              />
+              <i class="fa-solid fa-globe"></i>
+              <p>English</p>
+              <i class="fa-solid fa-caret-down"></i>
             </div>
+            <button>Sign In</button>
+          </div>
+        </div>
+        <div className="home-top-mid">
+          <div>
+            <h1>Unlimited movies, TV shows and more.</h1>
+            <p>Watch anywhere. Cancel anytime.</p>
+          </div>
+          <div>
+            <p>
+              Ready to watch? Enter your email to create or restart your
+              membership.
+            </p>
             <div>
-                <div>
-                    
-                </div>
-                <button>Sign In</button>
+                <input type="email" placeholder="Email Address"/>
+                <button>Get Started <i class="fa-solid fa-angle-right"></i></button>
             </div>
           </div>
         </div>
+      </div>
     </div>
   );
 }
